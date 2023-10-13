a8db8d5335622483b48a0c39d14d42511bdc3dc9
Arweenn <arwendumont25@outlook.fr>
2023-10-13 03:53:05 -0700

    4-puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,2 +1,14 @@
-#!/bin/bash
-gcc puts(Programming is like building a multilingual puzzle)
+#include <stdio.h>
+
+/**
+ * main -Entry point
+ *
+ * Description: prints "Programming is like building a multilingual puzzle"
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{ 
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
