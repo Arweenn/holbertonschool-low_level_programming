2be85029653957bb89c8d1fe3f191a66ed998e33
Arweenn <arwendumont25@outlook.fr>
2023-10-16 02:22:48 -0700

    0-positive_or_negative.c

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -9,6 +9,12 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	printf("%d \n", x);
+	if (n == 0)
+		printf("%d is zero\n", n);
+	else if (n < 0)
+		printf("%d is negative", n);
+	else 
+		printf ("%d is positive", n);
+
 	return (0);
 }
