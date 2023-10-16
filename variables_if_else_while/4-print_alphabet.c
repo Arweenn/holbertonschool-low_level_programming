3b8c895635c73245793d414649dd4ab619234bba
Arweenn <arwendumont25@outlook.fr>
2023-10-16 07:07:22 -0700

    4-print_alphabet.c

---
+++ variables_if_else_while/4-print_alphabet.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - print the alphabet
+ *
+ * Description : print the alphabet in lowercase, without q and e
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 97;
+
+	while (i < 123)
+	{
+		if ((i != 101) && (i != 113))
+		{putchar(i);
+		}
+		i++;
+	}
+	putchar(10);
+	return (0);
+}
