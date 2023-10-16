2e4dfb2cbe070b2250a83b4c45a222b7a34483c0
Arweenn <arwendumont25@outlook.fr>
2023-10-16 07:33:25 -0700

    7-print_tebahpla.c

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - print alphabet
+ *
+ * Description : print alphabet in lowercase + reverse
+ *
+ * Return: Always  0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 122;
+
+	while (i > 97)
+	{
+		putchar(i);
+		i++;
+	}
+	putchar(10)
+	return (0);
+}
