9e7effb83e8a2f5e43c9f844bb0474f79682eecc
Arweenn <arwendumont25@outlook.fr>
2023-10-16 07:18:50 -0700

    5-print_numbers.c

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - print all single digit numbers
+ *
+ * Description : print all single digit numbers, base of 10 from 0
+ *
+ * Return: Always  0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0
+
+	while (i < 10)
+	{
+		putchar(i);
+		i++;
+	}
+	putchar(10);
+	return (0);
