f962e133aaf1ccd8d53459872a6a2dec6f7b7530
Arweenn <arwendumont25@outlook.fr>
2023-10-16 06:43:09 -0700

    3-print_alphabets.c

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - print the alphabet
+ *
+ * Description : print the alphabet in lower then uppercase
+ *
+ * Return: Always  0 (Success)
+ */
+
+int main(void)
+{
+	int i, j;
+
+	i = 97;
+	j = 65;
+
+	while (i < 123)
+	{
+		putchar(i);
+		i++;
+	}
+	while (j < 91)
+	{
+		putchar(j);
+		j++;
+	}
+	putchar(10);
+	return(0);
+}
