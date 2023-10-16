09962359528517752fc829f3f6bc3e53f77fa877
Arweenn <arwendumont25@outlook.fr>
2023-10-16 04:53:54 -0700

    1-last_digit.c

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -14,7 +14,7 @@ int main(void)
 		printf("%d and is greater than 5\n", n);
 	if (n == 0)
 		printf("%d and is 0\n", n);
-	if (n < 6) && (n != 0);
+	if (n < 6)&&(n != 0);
 		printf("%d and is less than 6 and not 0\n", n);
 	return (0);
 }
