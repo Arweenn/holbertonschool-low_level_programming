afebf4da49b0ac30d82ccfe76b06249bf6ac7819
Arweenn <arwendumont25@outlook.fr>
2023-10-17 05:00:16 -0700

    2-print_alphabet_x10.c

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
-  print_alphabet_x10 - print alphabet x10
+ * print_alphabet_x10 - print alphabet x10
  *
  * Description : print alphabet in lowercase
  *
@@ -12,13 +12,12 @@ void print_alphabet_x10(void)
 	char c = 'a';
 	int i = 0;
 
-	while (i = 10)
-	
-		c = 'a';
-		while (i < 'z')
+	while (i < 10)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
 			putchar(c);
-			c++;
-	}
+		}
 	putchar("\n");
 	i++;
 	}
