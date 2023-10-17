357fd5bdd014999089edcd43705048abf4780698
Arweenn <arwendumont25@outlook.fr>
2023-10-17 04:52:16 -0700

    2-print_alphabet_x10.c

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -12,11 +12,12 @@ void print_alphabet_x10(void)
 	char c;
 	int i = 0;
 
-	while (i <= 9)
-	{
-	for (c = 'a'; c <= 'z'; c++);
-	{
-		putchar(c);
+	while (i = 10)
+	
+		c = 'a';
+		while (i < 'z')
+			putchar(c);
+		c++;
 	}
 	putchar("\n");
 	i++;
