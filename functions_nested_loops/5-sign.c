529480fe55fe5bb4e639a0279144b87c5ea90a7e
Arweenn <arwendumont25@outlook.fr>
2023-10-17 07:39:21 -0700

    5-sign.c

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -12,17 +12,17 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		_putchar(+)
+		_putchar('+');
 		return (1);
 	}
 	if (n = 0)
 	{
-		_putchar(0)
+		_putchar('0');
 		return (0);
 	}
 	if (n < 0)
 	{
-		_putchar(-)
+		_putchar('-');
 		return (-1);
 	}
 }
