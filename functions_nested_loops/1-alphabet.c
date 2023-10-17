0ed22f9dc538d88165e626d896494ab9becb2e35
Arweenn <arwendumont25@outlook.fr>
2023-10-17 02:20:37 -0700

    1-alphabet.c

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
- * main - print _putchar
+ * main - print alphabet
  *
- * Description : print _putchar
+ * Description : print alphabet in lowercase
  *
  * Return: Always  0 (Success)
  */
