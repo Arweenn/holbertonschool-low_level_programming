98f7331dfd487bba909557955948ed8880ff4cce
Arweenn <arwendumont25@outlook.fr>
2023-10-17 07:37:32 -0700

    5-sign.c

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _isalpha - print sign
+ *@c: c is an ascii character
+ *
+ * Return: 1 (if positive), 0 (if zero), -1 (if negative)
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(+)
+		return (1);
+	}
+	if (n = 0)
+	{
+		_putchar(0)
+		return (0);
+	}
+	if (n < 0)
+	{
+		_putchar(-)
+		return (-1);
+	}
+}
