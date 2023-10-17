a5389a8ab297ebf0782e5fd8d7b4632fb238b229
Arweenn <arwendumont25@outlook.fr>
2023-10-17 01:38:50 -0700

    0-putchar.c

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -10,15 +10,8 @@
 int main(void)
 
 {
-	printf("_\n");
-	printf("p\n");
-	printf("u\n");
-	printf("t\n");
-	printf("c\n");
-	printf("h\n");
-	printf("a\n");
-	printf("r\n");
-	printf("\n");
+	printf("_putchar\n");
 
+	printf("\n");
 	return (0);
 }
