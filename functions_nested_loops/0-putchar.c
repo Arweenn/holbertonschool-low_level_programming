f4ef5ee5da1ab26353042fda1d7421fafbccc944
Arweenn <arwendumont25@outlook.fr>
2023-10-17 01:33:45 -0700

    0-putchar.c

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,24 @@
+#include <main.h>
+/**
+ * main - print _putchar
+ *
+ * Description : print _putchar
+ *
+ * Return: Always  0 (Success)
+ */
+
+int main(void)
+
+{
+	printf("_\n");
+	printf("p\n");
+	printf("u\n");
+	printf("t\n");
+	printf("c\n");
+	printf("h\n");
+	printf("a\n");
+	printf("r\n");
+	printf("\n");
+
+	return (0);
+}
