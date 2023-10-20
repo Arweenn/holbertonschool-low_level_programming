70bbb5fb32e7c37ad9cf98c1b0ce04371138e6de
Arweenn <arwendumont25@outlook.fr>
2023-10-20 08:27:50 -0700

    .

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_diagonal - draw a diagonal line
+ * @n: integer
+ *
+ * Description: function that draws a diagonal line
+ *
+ * Return:void
+ */
+
+void print_diagonal(int n)
+{
