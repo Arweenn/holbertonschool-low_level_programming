9477dd7df4aae84fe908960f899b8348076337bf
Arweenn <arwendumont25@outlook.fr>
2023-10-20 06:24:20 -0700

    2-mul.c

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * mul - mutiply two integers
+ * @a: integer a
+ * @b: integer b
+ *
+ * Description: function that multiplies two integers
+ *
+ * Return: sum
+ */
+
+int mul(int a, int b)
+{
+	int sum;
+
+	sum = a * b;
+
+	return (sum);
+}
