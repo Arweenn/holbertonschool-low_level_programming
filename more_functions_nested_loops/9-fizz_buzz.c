f40f97a64ec172d219b99d6678398cc9c56777da
Arweenn <arwendumont25@outlook.fr>
2023-10-21 14:32:02 -0700

    9-fizz_buzz.c

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -13,17 +13,17 @@ int main(void)
 
 	for (n = 1; n < 101; n++)
 	{
-		if (n % 3 == 0)
+		if (n % 3 == 0 && n % 5 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if (n % 3 == 0)
 		{
 			printf("Fizz ");
 		}
 		else if (n % 5 == 0)
 		{
 			printf("Buzz ");
-		}
-		else if (n % 3 == 0 && n % 5 == 0)
-		{
-			printf("FizzBuzz ");
 		}
 		else
 		{
