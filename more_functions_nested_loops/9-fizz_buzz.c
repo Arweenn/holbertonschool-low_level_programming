235427ae5a5cc56e01341a23dd6d131acf1a548d
Arweenn <arwendumont25@outlook.fr>
2023-10-21 14:35:54 -0700

    9-fizz_buzz.c

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -23,7 +23,14 @@ int main(void)
 		}
 		else if (n % 5 == 0)
 		{
-			printf("Buzz ");
+			if (n == 100)
+			{
+				printf("Buzz");
+			}
+			else
+			{
+				printf("Buzz ");
+			}
 		}
 		else
 		{
