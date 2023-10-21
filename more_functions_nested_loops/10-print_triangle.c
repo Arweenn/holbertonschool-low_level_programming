34ac36b389262236be55680834aa237527a7cc58
Arweenn <arwendumont25@outlook.fr>
2023-10-21 14:57:19 -0700

    10-print_triangle.c

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints a triangle
+ * @size: size of the triangle
+ *
+ * Return: void
+ */
+
+void print_triangle(int size)
+{
+	int l = 0;
+	int s;
+	int h;
+
+	if (size > 0)
+	{
+		while (l < size)
+		{
+			for (s = size -1; s > l; s--)
+			{
+				_putchar(' ');
+			}
+			for (h = 0; h < l + 1; h++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+			l++;
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
