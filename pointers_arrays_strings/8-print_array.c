be5e458d5f69dc9fd75a1f4c73215f0025b4f152
Arweenn <arwendumont25@outlook.fr>
2023-10-24 07:48:55 -0700

    8-print_array.c

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - prints number of elements of an array
+ * @a: array
+ * @n: number of elements to print
+ * Return: void
+ */
+
+void print_array(int *a, int n)
+{
+	int i, count;
+
+	i = 0;
+	count = 1;
+	if (n < 0)
+		n = 0;
+	if (n > 0)
+	{
+		while (a[i] != '\0' && count < n)
+		{
+			printf("%d, ", a[i]);
+			i++;
+			count++;
+		}
+		printf("%d\n", a[i]);
+	}
+}
