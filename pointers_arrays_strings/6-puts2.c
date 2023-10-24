0a48c614774ae78845e8d3094f7f5a5fd774baa2
Arweenn <arwendumont25@outlook.fr>
2023-10-24 04:38:33 -0700

    6-puts2.c

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other character of a string starting with the first
+ * @str: is a string
+ * Return: void
+ */
+
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i+=2)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
