085309895af49b62f9cccc2838d9c3f643f66b34
Arweenn <arwendumont25@outlook.fr>
2023-10-25 07:51:26 -0700

    0-strcat.c

--- pointers_arrays_strings/0-main.c
+++ pointers_arrays_strings/0-main.c
@@ -8,11 +8,15 @@
  */
 int main(void)
 {
-	int n;
+    char s1[98] = "Hello ";
+    char s2[] = "World!\n";
+    char *ptr;
 
-	n = 402;
-	printf("n=%d\n", n);
-	reset_to_98(&n);
-	printf("n=%d\n", n);
-	return (0);
+    printf("%s\n", s1);
+    printf("%s", s2);
+    ptr = _strcat(s1, s2);
+    printf("%s", s1);
+    printf("%s", s2);
+    printf("%s", ptr);
+    return (0);
 }
---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strcat - appends the src string to the dest string
+ * @
+ * Return: resulting string dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i = 0, dest_len = 0;
+
+	while (dest[i++])
+		dest_len++;
+
+	for (i = 0; src[i]; i++)
+		dest[dest_len++] = src[i];
+
+	return (dest);
+}
