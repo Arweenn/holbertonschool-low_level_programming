adcb426c3a1b39980be6357d349505bcb081ba47
Arweenn <arwendumont25@outlook.fr>
2023-10-28 14:17:34 -0700

    Update main.h

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,3 +1,7 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stddef.h>
+
 int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
@@ -10,6 +14,7 @@ void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
+
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
@@ -18,3 +23,16 @@ void reverse_array(int *a, int n);
 char *string_toupper(char *);
 char *cap_string(char *);
 char *leet(char *);
+
+
+void *_memset(void *s, int c, size_t n);
+char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
+unsigned int _strspn(char *s, char *accept);
+char *_strpbrk(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
+void print_chessboard(char (*a)[8]);
+void print_diagsums(int *a, int size);
+void set_string(char **s, char *to);
+
+#endif
