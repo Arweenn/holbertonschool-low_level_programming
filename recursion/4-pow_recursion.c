ec3c43dad6eef9dc9658933c236f3af61529d342
Arweenn <arwendumont25@outlook.fr>
2023-10-30 04:04:07 -0700

    4-pow_recursion.c

---
+++ recursion/4-pow_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - function that returns the value of x raised to the power of y
+ * @x: base
+ * @y: exponent
+ * Return: value of the exponentiation
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	else if (y == 0)
+		return (1);
+
+	else
+		return (x * _pow_recursion(x, y - 1));
+}
