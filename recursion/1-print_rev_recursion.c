06f7d31d8cce20208bec6ed41abfc0c6f21870bc
Arweenn <arwendumont25@outlook.fr>
2023-10-30 14:23:41 -0700

    1-print_rev_recursion.c

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -3,18 +3,13 @@
 /**
  * _print_rev_recursion - prints a string in reverse using recursion
  * @s: string to reverse
- * Return: void
  */
 
 void _print_rev_recursion(char *s)
 {
-	if (*s == '\0')
+	if (*s != '\0')
 	{
-		_putchar('\n');
-		return;
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
 	}
-	s++;
-	_print_rev_recursion(s);
-	s--;
-	_putchar(*s);
 }
