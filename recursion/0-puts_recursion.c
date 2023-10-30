a96a5b51f0321b1088f7d63c225ae3ba04a9837d
Arweenn <arwendumont25@outlook.fr>
2023-10-30 03:10:14 -0700

    0-puts_recursion.c

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - function that prints a string
+ * @s: string to print
+ *
+ * Return: void
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	_putchar(*s);
+	s++;
+	_puts_recursion(s);
+}
