ea1547b7ab04329ff01d776ebbf090bb59b871ce
Arweenn <arwendumont25@outlook.fr>
2023-11-07 13:33:08 -0800

    main.h

--- more_malloc_free/main.h
+++ more_malloc_free/main.h
@@ -2,6 +2,7 @@
 #define MAIN_H
 #include <stddef.h>
 
+int _putchar(char c);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
