fad12c39ad418721ef4ac2a600208b208430130d
Arweenn <arwendumont25@outlook.fr>
2023-11-09 05:05:36 -0800

    3-function_like_macro.h

--- preprocessor/3-function_like_macro.h
+++ preprocessor/3-function_like_macro.h
@@ -1,4 +1,4 @@
 #ifndef FUNCTION_LIKE_MACRO_H
 #define FUNCTION_LIKE_MACRO_H
-#define ABS(num) ((num) > (-1) ? (num) : (num * -1))
-#endif
+#define ABS(x) ((x) > (-1) ? (x) : (x * -1))
+#endif /* !FUNCTION_LIKE_MACRO_H */
