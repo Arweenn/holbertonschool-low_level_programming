61234778e40e5eb9272f30b12044d6e27784fedd
Arweenn <arwendumont25@outlook.fr>
2023-11-27 06:02:22 -0800

    lists.h

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -13,9 +13,9 @@
  */
 typedef struct list_s
 {
-    char *str;
-    unsigned int len;
-    struct list_s *next;
+	char *str;
+	unsigned int len;
+	struct list_s *next;
 } list_t;
 
 int _putchar(char c);
