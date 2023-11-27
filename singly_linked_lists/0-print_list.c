0eb39c834f7211dad8880edccd45795919d923fd
Arweenn <arwendumont25@outlook.fr>
2023-11-27 05:47:06 -0800

    0-print_list.c

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_list - prints all elements of a list
+ * @h: the list_t list
+ * Return: number of nodes
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t nodes = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		nodes++;
+		h = h->next;
+	}
+
+	return (nodes);
+}
