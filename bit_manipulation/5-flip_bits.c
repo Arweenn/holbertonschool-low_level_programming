08836321a1c36a43f0a950a64be6405d906c50cb
Arweenn <arwendumont25@outlook.fr>
2023-11-30 06:46:21 -0800

    completed tasks

---
+++ bit_manipulation/5-flip_bits.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * flip_bits - gets the number of bits you need to flip from one num to another
+ * @n: number 1
+ * @m: number 2
+ * Return: number of bits
+ */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned int nbits;
+
+	for (nbits = 0; n || m; n >>= 1, m >>= 1)
+	{
+		if ((n & 1) != (m & 1))
+			nbits++;
+	}
+
+	return (nbits);
+}
