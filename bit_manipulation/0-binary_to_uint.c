d55a8a0a26488fd0f1f71930a8bde82bcc85f61d
Arweenn <arwendumont25@outlook.fr>
2023-11-30 01:36:39 -0800

    completed tasks

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -8,23 +8,28 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int num = 0, mult = 1;
-	int len;
+	unsigned int num;
+	int len, base_two;
 
 	if (*b == '\0')
 		return (0);
 
-	for (len = 0; b[len];)
-		len++;
+	num = 0;
 
-	for (len -= 1; len >= 0; len--)
+	for (len = 0; b[len] != '\0'; len++)
+		;
+
+	for (len--, base_two = 1; len >= 0; len--, base_two *= 2)
 	{
 		if (b[len] != '0' && b[len] != '1')
+		{
 			return (0);
+		}
 
-		num += (b[len] - '0') * mult;
-		mult *= 2;
+		if (b[len] & 1)
+		{
+			num += base_two;
+		}
 	}
-
 	return (num);
 }
