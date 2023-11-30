799db1538e17861a115d242d8f4a350cb96dde8d
Arweenn <arwendumont25@outlook.fr>
2023-11-30 01:56:23 -0800

    completed tasks

---
+++ bit_manipulation/1-print_binary.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_binary - prints the binary representation of a number
+ * @n: unsigned long integer
+ * Return: void
+ */
+
+void print_binary(unsigned long int n)
+{
+	if (n >> 0)
+	{
+		if (n >> 1)
+			print_binary(n >> 1);
+		_putchar((n & 1) + '0');
+	}
+	else
+		_putchar('0');
+}
