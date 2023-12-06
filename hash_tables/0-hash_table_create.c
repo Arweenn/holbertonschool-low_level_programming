0174f5404531fcb0a1b94cc3e9eca3561ee70387
Arweenn <arwendumont25@outlook.fr>
2023-12-06 14:56:13 -0800

    update

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -8,23 +8,19 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *table;
-	hash_node_t **array;
+	hash_table_t *ht;
 	unsigned long int i;
 
-	table = malloc(sizeof(hash_table_t));
-	if (table == NULL)
+	ht = malloc(sizeof(hash_table_t));
+	if (ht == NULL)
 		return (NULL);
 
-	array = malloc(sizeof(hash_node_t *) * size);
-	if (array == NULL)
+	ht->size = size;
+	ht->array = malloc(sizeof(hash_node_t *) * size);
+	if (ht->array == NULL)
 		return (NULL);
-
 	for (i = 0; i < size; i++)
-		array[i] = NULL;
+		ht->array[i] = NULL;
 
-	table->array = array;
-	table->size = size;
-
-	return (table);
+	return (ht);
 }
