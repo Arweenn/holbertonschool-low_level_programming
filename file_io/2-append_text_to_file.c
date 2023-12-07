48de730a4a41f8c0ae1437b824f7b9eb1075aa2f
Arweenn <arwendumont25@outlook.fr>
2023-12-07 03:33:29 -0800

    update

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -14,16 +14,16 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
+	 fd = open(filename, O_WRONLY | O_APPEND);
+
+        if (fd == -1)
+                return (-1);
+
 	if (text_content != NULL)
 	{
 		for (nletters = 0; text_content[nletters];)
 			nletters++;
 	}
-
-	fd = open(filename, O_WRONLY | O_APPEND);
-
-	if (fd == -1)
-		return (-1);
 
 	rwr = write(fd, text_content, nletters);
 
