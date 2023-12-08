b3e0118fc4a4dd0a4b34cca6ca13592c04224383
Arweenn <arwendumont25@outlook.fr>
2023-12-08 01:03:37 -0800

    update

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -19,8 +19,8 @@ hash_table_t *hash_table_create(unsigned long int size)
 	ht->array = malloc(sizeof(hash_node_t *) * size);
 	if (ht->array == NULL)
 		return (NULL);
-	/*for (i = 0; i < size; i++)
-		ht->array[i] = NULL;*/
+	for (i = 0; i < size; i++)
+		ht->array[i] = NULL;
 
 	return (ht);
 }
