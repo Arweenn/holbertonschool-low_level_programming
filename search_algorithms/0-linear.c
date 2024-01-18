44abcb94d5ed5de8aedccd499e2001980a57b531
Arweenn <arwendumont25@outlook.fr>
2024-01-18 12:36:08 +0100

    bettyfix

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -9,21 +9,21 @@
  * 
  * Return: the first index where value is located
  *         if value is not present in array or if array is NULL, return -1
-*/
+ */
 
 int linear_search(int *array, size_t size, int value)
 {
-    size_t i;
+	size_t i;
 
-    if (array == NULL)
-        return (-1);
+	if (array == NULL)
+		return (-1);
 
-    for (i = 0; i < size; i++)
-    {
-        printf("Value checked array[%lu] = [%d]\n", i, array[i]);
-        if (array[i] == value)
-            return (i);
-    }
+	for (i = 0; i < size; i++)
+	{
+		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+			return (i);
+	}
 
-    return (-1);
+	return (-1);
 }
